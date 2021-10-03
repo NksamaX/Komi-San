23c1866d2cffa749324abfa76361446c7b65c16b
Yαƚσ <75163978+Nksama@users.noreply.github.com>
2021-10-03 18:06:40 +0530

    the help module i messed up

--- nksama/plugins/helpers.py
+++ nksama/plugins/helpers.py
@@ -1,4 +1,6 @@
 
+from os import name
+from pyrogram.methods import messages
 from nksama import bot , help_message
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton , InlineKeyboardMarkup
@@ -30,22 +32,39 @@ def is_admin(group_id: int, user_id: int):
 @bot.on_callback_query(call_back_in_filter('help'))
 def callback_help(_,query):
 
-    for x in help_message:
-            global helptext_
-            helptext_ = x['Help']
+    # for x in help_message:
+    #         global helptext_
+    #         helptext_ = x['Help']
+    #         print(helptext_)
+
+    
+        
+
         
     if not query.data == "help":
-        msg = query.message
-        module = query.data.split(':')[1]
-        txt =  helptext_
+        try:
+            for x in help_message:
+                module = query.data.split(':')[1]
+                helptext_ = x[f'{module}_Help']
+                # helptext_ = x['Meme_Help']
+                query.message.reply(helptext_)
+                # query.message.reply(x['Admin_Help'])
 
-        msg.edit(txt)
+            msg = query.message
+            callback_module_name = query.data.split(':')[1]
+            txt =  helptext_
+
+            query.message.edit(txt)
+        except Exception as e:
+            query.message.reply(e)
     
     elif query.data == "help":
-        
-            query.message.edit("Commands and help" , reply_markup=InlineKeyboardMarkup([ 
-                [InlineKeyboardButton(x['Module_Name'], callback_data=f"help:{x['Module_Name']}")],
-            ]))
+        keyboard = []
+        for x in help_message:
+            keyboard.append([InlineKeyboardButton(x['Module_Name'], callback_data=f"help:{x['Module_Name']}")])
+
+        query.message.edit("Commands and Help" , reply_markup=InlineKeyboardMarkup(keyboard))
+            
 
 
     
