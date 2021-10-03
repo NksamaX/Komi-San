a9d21d3f75629fc80b289a24434594ed13e73742
Yαƚσ <75163978+Nksama@users.noreply.github.com>
2021-10-03 18:04:44 +0530

    Create notes.py

---
+++ nksama/plugins/notes.py
@@ -0,0 +1,67 @@
+from nksama.db import database as db
+from nksama import bot
+from pyrogram import filters
+import json
+from nksama import help_message
+
+@bot.on_message(filters.command('addnote'))
+def addnote(_,message):
+    note = message.text.split(' ')[1]
+    text = message.text.replace(message.text.split(" ")[0] , "").replace(message.text.split(' ')[1] , "")
+    db.insert_one(
+        {
+            "type": "note",
+            "chat_id": message.chat.id,
+            "note_name": note,
+            "text": text
+        }
+    )
+
+    message.reply_text('Added!')
+
+
+@bot.on_message(filters.command('getnote'))
+def get_note(_,message):
+    try:
+        note = message.text.split(' ')[1]
+        data = db.find_one({"chat_id": message.chat.id , "type": "note" , "note_name": note})
+        message.reply(data['text'])
+    except Exception as e:
+        message.reply(e)
+
+
+@bot.on_message(filters.command('notes'))
+def notes(_,message):
+    notes = None
+
+    # if notes:
+    #     for x in db.find({"chat_id": message.chat.id}):
+    #         notes = f"{notes}\n{x}"
+    #         message.reply(notes)
+    # else:
+    #     for x in db.find({"chat_id": message.chat.id}):
+    #         notes = x
+
+    for x in db.find({"chat_id": message.chat.id}):
+        if notes:
+            notes = f"{notes}\n{x['note_name']}"
+        
+        else:
+            notes = x['note_name']
+    
+    message.reply(notes)
+
+    
+
+@bot.on_message(filters.command('delnote'))
+def delnote(_,message):
+    note = message.text.split(" ")[1]
+    db.delete_one({"note_name": note})
+    message.reply('Deleted!')
+    
+
+
+help_message.append({
+    "Module_Name": "Notes",
+    "Notes_Help": "/addnote note name text - to add a note\n/delnote NoteName - to delete a note\ngetnote NoteName - get a note\n/notes - to get a list of notes in your chats"
+})
