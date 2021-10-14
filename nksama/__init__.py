ca19fe547c8cca80e3b86de3d30e665848cf334a
Yαƚσ <75163978+Nksama@users.noreply.github.com>
2021-10-14 13:38:55 +0530

    Update __init__.py

--- nksama/__init__.py
+++ nksama/__init__.py
@@ -5,7 +5,7 @@ bot = Client(
     'bot',
     api_id=os.environ.get('API_ID'),
     api_hash=os.environ['API_HASH'],
-    bot_token=os.environ['BOT_TOKEN'],
+    bot_token=os.environ['TOKEN'],
     plugins=dict(root=f"{__name__}/plugins")
 )
 
