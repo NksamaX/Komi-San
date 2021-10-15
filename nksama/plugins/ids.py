686b34fc2aefe7f42a3f6d4310522d8928221841
Yαƚσ <75163978+Nksama@users.noreply.github.com>
2021-10-15 11:49:07 +0530

    Update ids.py

--- nksama/plugins/ids.py
+++ nksama/plugins/ids.py
@@ -4,7 +4,7 @@ from pyrogram import filters
 
 @bot.on_message(filters.command('id'))
 def ids(_,message):
-  reply = message.reply_to_message:
+  reply = message.reply_to_message
   if reply:
     message.reply_text(f"**Your id**: {message.from_user.id}\n**User id**: {reply.from_user.id}\n**chat id**: {message.chat.id}")
   else:
