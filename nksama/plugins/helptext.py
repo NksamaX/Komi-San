c6d38fb6b7be669e4754d8a974ebb78b27696d7e
Yαƚσ <75163978+Nksama@users.noreply.github.com>
2021-10-15 14:10:45 +0530

    Update helptext.py

--- nksama/plugins/helptext.py
+++ nksama/plugins/helptext.py
@@ -1,19 +1,21 @@
 
 
 class Help_Text:
-    admin_help = """
+    helpp = {
+        "admin_help": """
 /ban - reply to a user
 /unban user id or username
 /pin - reply to a message
 /unpin - reply to a message
 /id - to get the user id
 
-"""
-    meme_help = """
+""",
+    "meme_help" : """
 /rmeme - to get random anime memes
-"""
+""",
 
-    notes_help = '"/addnote note name text - to add a note\n/delnote NoteName - to delete a note\ngetnote NoteName - get a note or use #notename\n/notes - to get a list of notes in your chats'
+    "notes_help" : '"/addnote note name text - to add a note\n/delnote NoteName - to delete a note\ngetnote NoteName - get a note or use #notename\n/notes - to get a list of notes in your chats',
 
-    extra_help = "/watchorder anime name - to get watchorder"
+    "extra_help" : "/watchorder anime name - to get watchorder",
     
+    }
