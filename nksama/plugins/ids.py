b7a2c03df64cc5ecabbd022735b0d42310c1633e
Yαƚσ <75163978+Nksama@users.noreply.github.com>
2021-10-15 11:48:00 +0530

    Create ids.py

---
+++ nksama/plugins/ids.py
@@ -0,0 +1,11 @@
+from nksama import bot
+from pyrogram import filters
+
+
+@bot.on_message(filters.command('id'))
+def ids(_,message):
+  reply = message.reply_to_message:
+  if reply:
+    message.reply_text(f"**Your id**: {message.from_user.id}\n**User id**: {reply.from_user.id}\n**chat id**: {message.chat.id}")
+  else:
+    message.reply(f"**Your id**: {message.from_user.id}\n**chat id**: {message.chat.id}")
