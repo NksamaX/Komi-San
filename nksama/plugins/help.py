052ea1232dc07e3e7f7280712427305f19f9b359
Yαƚσ <75163978+Nksama@users.noreply.github.com>
2021-10-15 18:10:59 +0530

    Update help.py

--- nksama/plugins/help.py
+++ nksama/plugins/help.py
@@ -3,6 +3,8 @@ from pyrogram.types.bots_and_keyboards import callback_game
 from pyrogram.types.bots_and_keyboards.inline_keyboard_button import InlineKeyboardButton
 from pyrogram.types.bots_and_keyboards.inline_keyboard_markup import InlineKeyboardMarkup 
 from nksama import bot ,help_message
+from typing import List
+
 
 
 
