a2a2221bab96809294789d85abfda2c74d683a2e
Yαƚσ <75163978+Nksama@users.noreply.github.com>
2021-10-16 11:57:10 +0530

    Update __init__.py

--- nksama/__init__.py
+++ nksama/__init__.py
@@ -11,7 +11,11 @@ bot = Client(
     plugins=dict(root=f"{__name__}/plugins")
 )
 
-REDIS_DB = StrictRedis.from_url(REDIS_URI,decode_responses=True)
+REDIS_URI = os.environ.get("REDIS_URL")
+
+REDIS_DB = StrictRedis.from_url(REDIS_URI
+                                ,decode_responses=True
+                               )
 
 REDIS_DB.ping()
 
