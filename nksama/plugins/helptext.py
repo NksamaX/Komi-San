c6617af7c358f91360a9a3b9cf09f6334f50a18c
Yαƚσ <75163978+Nksama@users.noreply.github.com>
2021-10-16 10:08:54 +0530

    Update helptext.py

--- nksama/plugins/helptext.py
+++ nksama/plugins/helptext.py
@@ -18,7 +18,10 @@ class Help_Text:
     "notes_help" : '"• /addnote note __name text - to add a note__\n•/delnote __NoteName - to delete a note__\n•/getnote __NoteName - get a note or use #notename__\n• /notes - __to get a list of notes in your chats__',
 
     "extra_help" : """• /watchorder __anime name - to get watchorder__
-• /quote - __to get random anime quotes__""",
+• /quote - __to get random anime quotes__
+• /pat - __ to pat someone__
+• /invitelink - __get invitelink of the current chat__
+""",
         
     "welcome_help" : """• /setwelcome __welcome message - text for welcome__
 • /clearwelcome - __clear welcome message__""",
