5805c7f7e9ca1f60e632e1d82c15702a80c49f36
RedAura <86167689+MoeZilla@users.noreply.github.com>
2021-10-16 11:34:57 +0530

    Update __init__.py

--- nksama/__init__.py
+++ nksama/__init__.py
@@ -1,13 +1,21 @@
 from pyrogram import filters , Client
+from redis import StrictRedis
 import os 
 
 bot = Client(
     'bot',
+    REDIS_URI = os.environ.get("REDIS_URI"),
     api_id=os.environ.get('API_ID'),
     api_hash=os.environ['API_HASH'],
     bot_token=os.environ['BOT_TOKEN'],
     plugins=dict(root=f"{__name__}/plugins")
 )
+
+REDIS_DB = StrictRedis.from_url(REDIS_URLI decode_responses=True)
+
+REDIS_DB.ping()
+
+
 
 
 help_message = []
