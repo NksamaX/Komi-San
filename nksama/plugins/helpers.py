be1cfba8bf04bc14b11819de8ab56b522e7e7756
「 Rajkumar™ 」 <71130660+Awesome-RJ@users.noreply.github.com>
2021-10-17 20:08:16 +0530

    `if` statement can be simplified

--- nksama/plugins/helpers.py
+++ nksama/plugins/helpers.py
@@ -19,12 +19,7 @@ def call_back_in_filter(data):
 def is_admin(group_id: int, user_id: int):
     try:
         user_data = bot.get_chat_member(group_id, user_id)
-        if user_data.status == 'administrator' or user_data.status == 'creator':
-            # print(f'is admin user_data : {user_data}')
-            return True
-        else:
-            # print('Not admin')
-            return False
+        return bool(user_data.status == 'administrator' or user_data.status == 'creator')
     except:
         # print('Not admin')
         return False
