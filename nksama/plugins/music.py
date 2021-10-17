ae5fff03d0ff92ad7a3d7b99d7f6ea5717e415b8
Yαƚσ <75163978+Nksama@users.noreply.github.com>
2021-10-17 10:36:19 +0530

    Create music.py

---
+++ nksama/plugins/music.py
@@ -0,0 +1,27 @@
+from pyrogram import filters
+from nksama import bot
+import pytgcalls
+
+
+calls = pytgcalls.GroupCallFactory(bot).get_group_call()
+
+
+@bot.on_message(filters.command('play'))
+async def play(_,message):
+  reply = message.reply_to_message
+  if reply:
+    fk = await message.reply('Downloading....')
+    path = await reply.download()
+    await calls.join(message.chat.id)
+    await calls.start_audio(path)
+    await fk.edit('playing...')
+
+    
+@bot.on_message(filters.command('vplay'))
+async def vplay(_,message):
+  reply = message.reply_to_message
+  if reply:
+    path = reply.download()
+    await calls.join(message.chat.id)
+    await calls.start_video(path)
+    await fk.edit('playing...')
