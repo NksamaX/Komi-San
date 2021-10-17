4c96b9b08a2dd99284cc28bfcb69565aa493310f
5M4ÇK3Ř <46376370+H4CK3R-5M4CK3R@users.noreply.github.com>
2021-10-17 22:25:09 +0530

    Update start.py

--- nksama/plugins/start.py
+++ nksama/plugins/start.py
@@ -10,10 +10,3 @@ def start(_,message):
     bot.send_message(message.chat.id , "Hello there i'm Komi-San\nI'll help you to manage your groups" , reply_markup=InlineKeyboardMarkup([ 
         [InlineKeyboardButton('help' , callback_data="help")]
     ]))
-    
-@bot.on_message(filters.command('start_test') | filters.chat([-1001544622735, 1068352349]))
-def start(_,message):
-
-    bot.send_message(message.chat.id , "Hello there i'm Komi-San\nI'll help you to manage your groups" , reply_markup=InlineKeyboardMarkup([ 
-        [InlineKeyboardButton('help' , callback_data="help")]
-    ]))
