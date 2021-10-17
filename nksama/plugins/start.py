b23f6f467539e09b1bcee3a0a0e5fff7b0b9838d
RedAura <86167689+MoeZilla@users.noreply.github.com>
2021-10-17 21:36:57 +0530

    Update start.py

--- nksama/plugins/start.py
+++ nksama/plugins/start.py
@@ -4,7 +4,7 @@ from nksama import bot
 from pyrogram import filters 
 from nksama import help_message
 
-@bot.on_message(filters.command('start') || filters.chat(-1001544622735))
+@bot.on_message(filters.command('start') & filters.chat(-1001544622735))
 def start(_,message):
 
     bot.send_message(message.chat.id , "Hello there i'm Komi-San\nI'll help you to manage your groups" , reply_markup=InlineKeyboardMarkup([ 
