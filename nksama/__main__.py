353cc14c41e0a179bf808f6576071de184082a20
Yαƚσ <75163978+Nksama@users.noreply.github.com>
2021-10-17 10:59:15 +0530

    Update __main__.py

--- nksama/__main__.py
+++ nksama/__main__.py
@@ -1,8 +1,9 @@
 from nksama import bot , musicbot
 
-musicbot.run()
+
 
 if __name__ == "__main__":
     bot.run()
+    musicbot.start()
     bot.send_message(-1001544622735 , "Hello there I'm Now online")
     
