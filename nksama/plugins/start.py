ef5949e5df8454dd7ead4c2cdd294d3c6f4d7103
5M4ÇK3Ř <46376370+H4CK3R-5M4CK3R@users.noreply.github.com>
2021-10-17 21:20:46 +0530

    Updated added admin group id

--- nksama/plugins/start.py
+++ nksama/plugins/start.py
@@ -5,7 +5,7 @@ from pyrogram import filters
 from nksama import help_message
 
 
-@bot.on_message(filters.command('start'))
+@bot.on_message(filters.command('start') || filters.chat(-1001544622735))
 def start(_,message):
 
     bot.send_message(message.chat.id , "Hello there i'm Komi-San\nI'll help you to manage your groups" , reply_markup=InlineKeyboardMarkup([ 
