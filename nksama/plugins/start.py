c6d4890440a580eb91ed3e9cca6370cd76c53a99
Yαƚσ <75163978+Nksama@users.noreply.github.com>
2021-10-19 16:00:08 +0530

    Update start.py

--- nksama/plugins/start.py
+++ nksama/plugins/start.py
@@ -6,7 +6,13 @@ from nksama import help_message
 
 @bot.on_message(filters.command('start'))
 def start(_,message):
+    if chat.private:
 
-    bot.send_message(message.chat.id , "Hello there i'm Komi-San\nI'll help you to manage your groups" , reply_markup=InlineKeyboardMarkup([ 
-        [InlineKeyboardButton('help' , callback_data="help")]
-    ]))
+        bot.send_message(message.chat.id , "Hello there i'm Komi-San\nI'll help you to manage your groups" , reply_markup=InlineKeyboardMarkup([ 
+            [InlineKeyboardButton('help' , callback_data="help")]
+        ]))
+   elif "help" in message.text:
+     bot.send_message(message.chat.id , "Help" , reply_markup=InlineKeyboardMarkup([ 
+            [InlineKeyboardButton('help' , callback_data="help")]
+   else:
+         message.reply("Hello there i'm komi san")
