0e073f8f5ecf75dfa52c1170c2ad9ff9ff60aac6
Yαƚσ <75163978+Nksama@users.noreply.github.com>
2021-10-19 19:22:49 +0530

    Update stats.py

--- nksama/plugins/stats.py
+++ nksama/plugins/stats.py
@@ -2,7 +2,6 @@ from pyrogram import filters , Client
 from nksama import bot
 from pymongo import MongoClient 
 from nksama.db import MONGO_URL as db_url
-from nksama.plugins.start import grps
 
 users_db = MongoClient(db_url)['users']
 col = users_db['USER']
