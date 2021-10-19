f057be35ac59ec7b6c8b69a20835dfa3bfbe0d2d
Yαƚσ <75163978+Nksama@users.noreply.github.com>
2021-10-19 18:44:12 +0530

    Update stats.py

--- nksama/plugins/stats.py
+++ nksama/plugins/stats.py
@@ -1,24 +1,12 @@
 from pyrogram import filters , Client
 from nksama import bot
-from pymongo import MongoClient as client
-from nksama.db import MONGO_URL as url
+from pymongo import MongoClient 
+from nksama.db import MONGO_URL as db_url
 
-users_db = client(url)['users']
+users_db = MongoClient(db_url)['users']
 col = users_db['USER']
 
-@bot.on_message(filters.command("start"))
-def update_stats(_,message):
-  users = col.find({})
-  mfs = []
-  for x in users:
-    mfs.append(x['user_id'])
-  if message.from_user.id not in mfs:
-    user = {"type": "user" , "user_id": message.from_user.id}
-    users_db.insert_one(user)
-    
-    
-    
-    
+
 @bot.on_message(filters.command("stats"))
 def stats(_,message):
   users = col.find({})
@@ -28,7 +16,7 @@ def stats(_,message):
     
   total = len(mfs)
   
-  message.reply_text(f"Total Users: {total}")
+  bot.send_message(message.chat.id , f"Total Users: {total}")
   
 
 
