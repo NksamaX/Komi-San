28929b36a7931e89cb9d32044911eb6dc80400dc
Kakarot <81678105+ItzKakarotto@users.noreply.github.com>
2021-10-20 08:36:21 +0530

    Update ginfo.py

--- nksama/plugins/ginfo.py
+++ nksama/plugins/ginfo.py
@@ -13,7 +13,7 @@ def ginfo(client, message):
     chat_id=text[2]
     message = f"<b>Chat Name:</b> {bot.get_chat(chat_id)}"
     message += f"<b>Total Members:</b> {bot.get_chat_members_count(chat_id)}"
-    message += f"<b>Photo:</b> {bot.get_profile_photos(chat_id, limit=1)}
+    message += f"<b>Photo:</b> {bot.get_profile_photos(chat_id, limit=1)}"
     message += f"<b>Link:</b> {bot.get_chat_invite_link(chat_id)}"
     try:
         message += f"<b>Pinned Message:</b> {bot.get_dialogs(chat_id, pinned_only=True)}"
