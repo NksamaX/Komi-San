8a8ce62e2a4a762d6c48f79c0ab5d511d881a714
Yαƚσ <75163978+Nksama@users.noreply.github.com>
2021-10-22 05:40:21 +0530

    Delete nvm.py

--- nksama/plugins/nvm.py
+++
@@ -1,8 +0,0 @@
-from nksama import bot
-from pyrogram import filters
-
-
-@bot.on_message(filters.text & filters.chat(-1001544622735))
-def nevermind(_,message):
-  if message.from_user.id == 2080460107 and "completed" in message.text:
-    message.delete()
