424747818cc1e773b939418c23fdb0d468197a7c
Yαƚσ <75163978+Nksama@users.noreply.github.com>
2021-10-24 10:59:56 +0530

    Update telegraph.py

--- nksama/plugins/telegraph.py
+++ nksama/plugins/telegraph.py
@@ -1,34 +1,18 @@
-#test 
-import requests
-from pil import Image
-from nksama import bot
-import os
+
+from telegraph import upload_file
 from pyrogram import filters
+from nksama import bot
+
 
 @bot.on_message(filters.command('ul'))
 def ul(_,message):
-   if message.reply_to_message.media:
-        path = message.reply_to_message.download()
-        file = {
-            ("media", open(path, 'rb'))
-        }
+    reply = message.reply_to_message
+    if reply.media:
+        path = reply.download()
+        fk = upload_file(path)
+        for x in fk:
+            url = "https://telegraph.ph" + x
+        
+        message.reply_text(url)
 
-        res = requests.post("https://telegra.ph/upload" , files=file)
 
-        for x in res.json():
-            if not "webp" in path:
-                link = x['src']
-                bot.send_message(message.chat.id , f"https://telegra.ph{link})")
-                os.remove(path)
-            else:
-                sticker = Image.open(path).convert("RGB")
-                sticker.save(f"{path}.jpg" , "jpeg")
-                file = {
-                    ("media", open(f"{path}.jpg" , 'rb'))
-                }
-                res = requests.post("https://telegra.ph/upload" , files=file)
-                print(res.text)
-                for x in res.json():
-                    link = x['src']
-                    bot.send_message(message.chat.id , f"https://telegra.ph{link}")
-                    os.remove(f"{path}.jpg")
