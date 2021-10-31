6fb42e817a618b2871065f07cdfb0f3ac10928c8
Yαƚσ <75163978+Nksama@users.noreply.github.com>
2021-10-31 09:19:48 +0530

    Update rename.py

--- nksama/plugins/rename.py
+++ nksama/plugins/rename.py
@@ -5,17 +5,20 @@ from pyrogram import filters
 
 @bot.on_message(filters.command('rename'))
 def rename(_,message):
+  
   try:
     filename = message.text.replace(message.text.split(" ")[0] , "")
     
   except Exception as e:
     send_log(e)
     
-  x = message.reply_text("Downloading.....")
+  
     
   reply = message.reply_to_message
   if reply:
+    x = message.reply_text("Downloading.....")
     path = reply.download(file_name=filename)
     x.edit("Uploading.....")
     message.reply_document(path)
     os.remove(path)
+  
