43647780cfe1e889f46e3deac604d171e3ceb8e2
Yαƚσ <75163978+Nksama@users.noreply.github.com>
2021-11-05 10:52:43 +0530

    Update helptext.py

--- nksama/plugins/helptext.py
+++ nksama/plugins/helptext.py
@@ -20,7 +20,7 @@ class Help_Text:
 • /rmeme - __to get random anime memes__
 """,
 
-    "notes_help" : '"• /addnote note __name text - to add a note__\n•/delnote __NoteName - to delete a note__\n•/getnote __NoteName - get a note or use #notename__\n• /notes - __to get a list of notes in your chats__',
+    "notes_help" : '• /addnote note __name text - to add a note__\n•/delnote __NoteName - to delete a note__\n•/getnote __NoteName - get a note or use #notename__\n• /notes - __to get a list of notes in your chats__',
 
     "extra_help" : """• /watchorder __anime name - to get watchorder__
 • /quote - __to get random anime quotes__
