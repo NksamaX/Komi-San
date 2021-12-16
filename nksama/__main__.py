6bcc7f74d87516681cf45ffbc15188e5b1170dab
Yαƚσ <75163978+Nksama@users.noreply.github.com>
2021-12-16 06:05:55 +0530

    Update __main__.py

--- nksama/__main__.py
+++ nksama/__main__.py
@@ -2,12 +2,21 @@ from nksama import bot , musicbot
 import logging
 
 
+logging.basicConfig(
+    filename="log.txt", level=logging.INFO,
+    filemode="a",
+    format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+    datefmt='%H:%M:%S',
+)
+
+def main ():
+    bot.run()
+    musicbot.start()
+    bot.send_message(-1001544622735 , "I'm Now online")
+    
 
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO)
-    bot.run()
-    musicbot.start()
-    with bot:
-        bot.send_message(-1001544622735 , "I'm Now online")
+    main()
+
     
