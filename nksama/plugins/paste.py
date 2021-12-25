18b70869de882f8d5ebcf404866b2d355db2e887
Yαƚσ <75163978+Nksama@users.noreply.github.com>
2021-12-25 08:19:51 +0530

    Create paste.py

---
+++ nksama/plugins/paste.py
@@ -0,0 +1,19 @@
+from requests import post , get
+from nksama import bot
+
+
+def paste(text):
+  url = "https://spaceb.in/api/v1/documents/"
+  res = post(url , data={"content": text , "extension":"txt"})
+  return f"https://spaceb.in/{res.json()['payload']['id']}"
+
+  
+@bot.on_message(filters.command('paste'))
+def pastex(_,message):
+  text = message.reply_to_message
+  if text:
+    x = paste(text.text)
+    message.reply(x)
+    
+  else:
+    message.reply_text("Reply to a message!")
