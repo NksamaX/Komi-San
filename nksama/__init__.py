2648386685299091493636c1cc01ca89720710d1
Yαƚσ <75163978+Nksama@users.noreply.github.com>
2021-12-27 13:45:03 +0530

    Update __init__.py

--- nksama/__init__.py
+++ nksama/__init__.py
@@ -8,16 +8,25 @@ help = Help_Text().helpp
 help_message = []
 
 
-class bot(Client):
-    super().__init__('bot',
+# class bot(Client):
+#     super().__init__(
+#         'bot',
+#                      api_id=os.environ.get('API_ID'),
+#                      api_hash=os.environ['API_HASH'],
+#                      bot_token=os.environ['BOT_TOKEN'],
+#                      plugins=dict(root=f"{__name__}/plugins"))
+
+#     def add_cmd(module, help):
+#         help_message.append({"Module_Name": module})
+#         help.update({f"{module}_help": help})
+
+
+bot = Client('bot',
                      api_id=os.environ.get('API_ID'),
                      api_hash=os.environ['API_HASH'],
                      bot_token=os.environ['BOT_TOKEN'],
-                     plugins=dict(root=f"{__name__}/plugins"))
-
-    def add_cmd(module, help):
-        help_message.append({"Module_Name": module})
-        help.update({f"{module}_help": help})
+                     plugins=dict(root=f"{__name__}/plugins")
+)
 
 
 PYRO_SESSION = os.environ['PYRO_SESSION']
